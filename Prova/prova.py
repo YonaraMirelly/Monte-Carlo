f7b62a36430a8eec5a81e52ec2de2bf4a6723bf9
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-09-27 09:21:09 -0300

    commit

--- Prova/prova.py
+++ Prova/prova.py
@@ -1,20 +1,14 @@
 import itertools
+baralho = [1] * 4 + list(range(2, 11)) * 4 + [10] * 4 * 3 
 
-# Definir os valores das cartas do baralho
-baralho = [1] * 4 + list(range(2, 11)) * 4 + [10] * 4 * 3  # Ases, cartas numeradas e figuras
-
-# Total de combinações possíveis
 total_combinacoes = len(list(itertools.combinations(baralho, 3)))
 
-# Contador de combinações que somam 21
 contador_soma_21 = 0
 
-# Contar todas as combinações de 3 cartas que somam 21
 for combinacao in itertools.combinations(baralho, 3):
     if sum(combinacao) == 21:
         contador_soma_21 += 1
 
-# Calcular a probabilidade
 probabilidade_exata = contador_soma_21 / total_combinacoes
 
 print(f"Número total de combinações: {total_combinacoes}")
